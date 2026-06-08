fe49fef095889847beb98f555d213757d0ea3495
Wildfell Hall <136401567+wildfellhall@users.noreply.github.com>
2026-06-07 20:37:06 -0500

    Add placeholder message with song recommendations

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-
+"""
+Coming soon! While you wait, do yourself a favor and listen to the following songs from this repository's namesake: 
+- Chloe or Sam or Sophia or Marcus
+- Peter
+- The Bolter
+- Clara Bow
+- Down Bad
+- Guilty as Sin? 
+- So Long, London
+- The Manuscript
+- The Prophecy
+- Robin
+- THE ENTIRE ALBUM AND THE ANTHOLOGY!!!
+"""
